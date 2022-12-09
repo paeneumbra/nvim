ca0f37b591264e138e8d257a807bd6bdfa6b1e0e
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2022-12-09 21:29:11 +0100

    NVIM: Update colorscheme config

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,7 +1,12 @@
+local colorscheme = "gruvbox"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+    return
+end
+
 vim.o.termguicolors = true
 vim.o.background = 'dark'
-vim.cmd('colorscheme gruvbox')
 vim.cmd([[
     hi Normal ctermbg=NONE guibg=NONE'
 ]])
-
