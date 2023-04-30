0620e5bec2fb44a91f07c09d3f89fa07d4957e26
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2023-04-30 17:22:28 +0200

    NVIM: Set new text margins to 120

--- lua/settings.lua
+++ lua/settings.lua
@@ -8,24 +8,26 @@ o.incsearch = true
 o.tabstop = 4
 o.shiftwidth = 4
 o.softtabstop = 4
-o.scrolloff = 6                                 -- minimum lines to keep arround cursor when scrolling
-o.clipboard = 'unnamedplus'                        -- copy to system clipboard - uses xclip
-o.backspace = { 'indent', 'eol', 'start' }    -- use backspace to delete i,o,s
-o.cmdheight = 2                                  -- height for displaying messages
-o.mouse = 'v'                                            -- middle-click paste
-o.termguicolors = true                                    -- enable 24 bit colors
-o.laststatus = 2                                        -- status line always
-o.completeopt = { 'menuone', 'noselect', 'preview' }    -- cmp popup options
+o.scrolloff = 6                                      -- minimum lines around cursor when scrolling
+o.clipboard = "unnamedplus"                          -- copy to system clipboard - uses xclip
+o.backspace = { "indent", "eol", "start" }           -- use backspace to delete i,o,s
+o.cmdheight = 2                                      -- height for displaying messages
+o.mouse = "v"                                        -- middle-click paste
+o.termguicolors = true                               -- enable 24 bit colors
+o.laststatus = 2                                     -- status line always
+o.completeopt = { "menuone", "noselect", "preview" } -- cmp popup options
 
 -- window local
-o.number = true                -- numbered lines
-o.relativenumber = true        -- displays current line number
-o.colorcolumn = '80'        -- set a column border
-o.cursorline = true         -- highlights the current line
-o.conceallevel = 0          -- markdown `` is visible again
+o.number = true         -- numbered lines
+o.relativenumber = true -- displays current line number
+-- does not really work on markdown, I opted to include config file for markdownlint where necessary
+o.textwidth = 120       -- set text width
+o.colorcolumn = "120"   -- set a column border matching textwidth
+o.cursorline = true     -- highlights the current line
+o.conceallevel = 0      -- markdown `` is visible again
 
 -- buffer local
-o.expandtab = true            -- expand tab to spaces so that tabs are spaces
-o.autoindent = true            -- indent a new line
-o.smartindent = true        -- indent a new line - extra stuff
-o.fileencoding = 'utf-8'
+o.expandtab = true   -- expand tab to spaces so that tabs are spaces
+o.autoindent = true  -- indent a new line
+o.smartindent = true -- indent a new line - extra stuff
+o.fileencoding = "utf-8"
