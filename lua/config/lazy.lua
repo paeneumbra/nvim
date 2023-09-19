ec06d67231eda1fcd80d9700c5b350bbf3a3559d
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2023-09-19 10:46:13 +0200

    chore(nvim): Update plugins and options

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,12 +10,14 @@ require("lazy").setup {
     spec = {
         -- add LazyVim and import its plugins
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+        { import = "lazyvim.plugins.extras.dap.core" },
         { import = "lazyvim.plugins.extras.lang.docker" },
         { import = "lazyvim.plugins.extras.lang.java" },
         { import = "lazyvim.plugins.extras.lang.json" },
         { import = "lazyvim.plugins.extras.lang.python" },
         { import = "lazyvim.plugins.extras.lang.terraform" },
         { import = "lazyvim.plugins.extras.lang.yaml" },
+        { import = "lazyvim.plugins.extras.text.core" },
         { import = "plugins" },
     },
     defaults = {
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,22 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.autoindent = true -- indent a new line
+opt.backspace = { "indent", "eol", "start" } -- use backspace to delete i,o,s
+opt.conceallevel = 0 -- markdown `` is visible again
+opt.fileencoding = "utf-8"
+opt.ignorecase = true
+opt.incsearch = true
+opt.number = true -- numbered lines
+opt.relativenumber = true -- displays current line number
+opt.scrolloff = 6 -- minimum lines around cursor when scrolling
+opt.shiftwidth = 4
+opt.smartcase = true
+opt.smartindent = true -- Insert indents automatically
+opt.softtabstop = 4
+opt.tabstop = 4
+opt.termguicolors = true -- enable 24 bit colors
+opt.wrap = false -- Disable line wrap
