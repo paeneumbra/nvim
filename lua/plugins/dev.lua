451aa17dc3643862f19010d8e007058852990e66
Simao Ferreira <simao.ferreira@zooplus.com>
2023-10-16 23:50:56 +0200

    chore: Add treesitter extra configuration

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -5,12 +5,35 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 
+local xdg_config = vim.env.XDG_CONFIG_HOME or vim.env.HOME .. "/.config"
+
+---@param path string
+local function have(path)
+    return vim.loop.fs_stat(xdg_config .. "/" .. path) ~= nil
+end
+
 return {
 
     {
         "nvim-treesitter/nvim-treesitter",
         opts = function(_, opts)
             vim.list_extend(opts.ensure_installed, {})
+
+            local function add(lang)
+                if type(opts.ensure_installed) ~= "table" then
+                    table.insert(opts.ensure_installed, lang)
+                end
+            end
+
+            vim.filetype.add {
+                extension = { rasi = "rasi" },
+            }
+
+            add "git_config"
+
+            if have "rofi" or have "wofi" then
+                add "rasi"
+            end
         end,
     },
 
