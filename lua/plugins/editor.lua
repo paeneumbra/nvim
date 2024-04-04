ebd0ff2a086dd5112769255d4b296704e00d93a3
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-04-04 21:16:18 +0200

    chore: Force telescope to search hidden files

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -104,4 +104,24 @@ return {
             },
         },
     },
+    {
+        "nvim-telescope/telescope.nvim",
+        -- opts will be merged with the parent spec
+        opts = {
+            defaults = {
+                file_ignore_patterns = { ".git/", "node_modules", "poetry.lock", ".idea/", ".venv/" },
+                vimgrep_arguments = {
+                    -- make sure hidden files are present
+                    "rg",
+                    "--color=never",
+                    "--no-heading",
+                    "--hidden",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                },
+            },
+        },
+    },
 }
