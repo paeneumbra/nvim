10b255eb39c580ddffb2f930edffea2812402953
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-12-16 11:40:16 +0100

    chore: Update plugins in the wake of lazyvim 14

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -8,8 +8,6 @@
 return {
     -- LSP outline functions window - <leader>cs to trigger
     { import = "lazyvim.plugins.extras.editor.outline" },
-    -- LSP navigation for lualine
-    { import = "lazyvim.plugins.extras.editor.navic" },
     -- Better yank (copy paste)
     { import = "lazyvim.plugins.extras.coding.yanky" },
     -- Better rename
@@ -24,26 +22,5 @@ return {
             local cmp = require "cmp"
             opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
         end,
-    },
-
-    {
-        "nvim-telescope/telescope.nvim",
-        -- opts will be merged with the parent spec
-        opts = {
-            defaults = {
-                file_ignore_patterns = { ".git/", "node_modules", "poetry.lock", ".idea/", ".venv/" },
-                vimgrep_arguments = {
-                    -- make sure hidden files are present
-                    "rg",
-                    "--color=never",
-                    "--no-heading",
-                    "--hidden",
-                    "--with-filename",
-                    "--line-number",
-                    "--column",
-                    "--smart-case",
-                },
-            },
-        },
-    },
+    }
 }
