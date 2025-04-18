ad7147c55f61599c9d1074afe7399f16fb2d99b8
Itzalak <24299428+itzalak@users.noreply.github.com>
2025-04-18 19:18:12 +0200

    tools: update pre commit and lua config

--- .luacheckrc
+++ .luacheckrc
@@ -1,1 +1,11 @@
-global = { "vim" }
+-- vim: ft=lua tw=80
+
+ignore = {
+    "122", -- Setting a read-only field of a global variable.
+    "212", -- Unused argument, In the case of callback function, _arg_name is easier to understand than _, so this option is set to off.
+}
+
+-- Global objects defined by the C code
+read_globals = {
+    "vim",
+}
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -15,9 +15,7 @@ return {
                 -- By default lazyvim only enables the formatter if a diagnostic is present,
                 -- It does not suit my use case, I want it always on.
                 ["markdownlint-cli2"] = {
-                    condition = function()
-                        return true
-                    end,
+                    condition = function() return true end,
                 },
             },
             formatters_by_ft = {
