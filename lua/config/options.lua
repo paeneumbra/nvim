52d51957b6df97088d0c33256007c60097cff144
Itzalak <24299428+itzalak@users.noreply.github.com>
2025-05-02 22:00:26 +0200

    chore: remove lazyvim defaults

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,20 +9,12 @@ local opt = vim.opt
 
 opt.autoindent = true -- indent a new line
 opt.backspace = { "indent", "eol", "start" } -- use backspace to delete i,o,s
-opt.conceallevel = 0 -- markdown `` is visible again
+opt.conceallevel = 0 -- Make markdown `` visible
 opt.fileencoding = "utf-8"
-opt.ignorecase = true
-opt.incsearch = true
-opt.number = true -- numbered lines
-opt.relativenumber = true -- displays current line number
 opt.scrolloff = 6 -- minimum lines around cursor when scrolling
 opt.shiftwidth = 4
-opt.smartcase = true
-opt.smartindent = true -- Insert indents automatically
 opt.softtabstop = 4
 opt.tabstop = 4
-opt.termguicolors = true -- enable 24 bit colors
-opt.wrap = false -- Disable line wrap
 
 -- Files
 vim.filetype.add {
